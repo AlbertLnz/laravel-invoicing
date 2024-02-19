0b41e70b6a4ac2d42906372bfe29e01f29dd0071
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-19 14:05:08 +0100

    Model & Migration of Company

---
+++ app/Models/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    //
+    protected $fillable = [
+        'entity_type',
+        'tin',
+        'direction',
+        'lpgo_path',
+        'root_user',
+        'root_password',
+        'client_id',
+        'client_secret',
+        'production',
+        'user_id'
+    ];
+}
---
+++ database/migrations/2024_02_19_124612_create_companies_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('entity_type');
+            $table->string('tin'); // Tax Identification Number
+            $table->string('direction');
+            $table->string('logo_path')->nullable();
+
+            // Credentials
+            $table->string('root_user');
+            $table->string('root_password');
+            $table->string('certificate_path');
+
+            // API Credentials
+            $table->string('client_id')->nullable();
+            $table->string('client_secret')->nullable();
+
+            $table->boolean('production')->default(false);
+
+            // Foreign key
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
